4505ee8470257816252983dd0616bc20569e8fc3
Bruno Tarmann <bruno@tarmann.com.br>
2015-06-17 17:20:10 +0100

    experimenting

--- app/app.js
+++ app/app.js
@@ -3,6 +3,9 @@ window.App = Ember.Application.create();
 // Router
 
 App.Router.map(function() {
+
+  this.route('timer')
+
   this.resource('user', function() {
     this.route('collection', { path: ':user' })
   });
@@ -12,41 +15,34 @@ App.Router.reopen({
   location: 'hash'
 });
 
+App.TimerRoute = Ember.Route.extend({
+  model: function(){
+    return [{ seconds: 3 }, { seconds: 20 }, { seconds: 40 }]
+  }
+});
+
 App.UserCollectionRoute = Ember.Route.extend({
   model: function(params) {
-    // return this.store.find('collection', params.user);
     return $.getJSON("http://bgg-json.azurewebsites.net/collection/" + params.user);
+    // return this.store.find('collection', params.user);
   }
 });
 
 // UserCollectionModel
 
-App.CollectionModel = DS.Model.extend({
-  name        : DS.attr(),
-  minPlayers  : DS.attr(),
-  maxPlayers  : DS.attr(),
-  isExpansion : DS.attr(),
-  owned       : DS.attr(),
-  rating      : DS.attr()
-});
+App.CollectionModel = DS.Model.extend();
 
 // UserCollectionAdapter
 
 App.CollectionAdapter = DS.RESTAdapter.extend({
-  // host: 'http://bgg-json.azurewebsites.net',
-  host: 'http://private-0f6a1-ember37.apiary-mock.com',
-  pathForType: function(type) { return 'collection'; }
+  host: 'http://bgg-json.azurewebsites.net'
+  // host: 'http://private-0f6a1-ember37.apiary-mock.com'
+  , pathForType: function(type) { return 'collection'; }
 });
 
 // UserCollectionSerializer
 
-App.CollectionSerializer = DS.RESTSerializer.extend({
-  primaryKey: 'gameId'
-  , serialize: function(snapshot, options){
-    console.log('snapshot', snapshot);
-    return { collections: snapshot };
-  }
-});
+App.CollectionSerializer = DS.RESTSerializer.extend({});
 
 // Controllers
 
@@ -54,13 +50,16 @@ App.IndexController = Ember.Controller.extend({
   actions: {
     go: function() {
       this.transitionToRoute('/user/' + this.user)
+    },
+    query: function() {
+      this.store.find('collection', 'tarmann').then(function(data){
+        console.log(data);
+      });
     }
   }
 });
 
 App.UserCollectionController = Ember.Controller.extend({
-  user: '',
-
   actions: {
     query: function() {
       this.store.find('collection', this.user).then(function(data){
